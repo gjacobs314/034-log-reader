4c77a936612ed7a934baf60ec2262eeea3797ebc
Gordon Jacobs <34612258+gjacobs314@users.noreply.github.com>
2022-03-04 13:22:30 -0500

    034 log shell

--- log-csv.py
+++ log-csv.py
@@ -1,9 +1,9 @@
-import enum
 import sys
 import time
 from datetime import datetime
 import pandas as pd
 import seaborn as sns
+sns.set_style("darkgrid")
 import collections
 import matplotlib.pyplot as plt
 import numpy as np
@@ -144,12 +144,14 @@ def loop(df):
         found_field = ""
         found_field_list = ""
         prompt_input = input("\n[S] SEARCH [P] PRINT [G] GRAPH : ")
-        lower = prompt_input.lower()
-        if lower == "all":
-            print_all_fields(df)
-            continue
         if prompt_input.lower() == "s":
             search_fields(df)
+            continue
+        if prompt_input.lower() == "p":
+            print_fields(df)
+            continue
+        if prompt_input.lower() == "g":
+            graph_fields(df)
             continue
         print("\nCommand not found...")
     
@@ -161,15 +163,13 @@ def search_fields(df):
     found_field = ""
     found_field_list = ""
     while True:
-        prompt_input = input("\n[X] ENTER FIELD INDEX: ")
+        prompt_input = input("\nENTER FIELD INDEX : ")
         for x, field in enumerate(all_logging_fields):
-            if prompt_input == field:
-                found_field, found_field_list = get_field(df, field)
             try:
                 if int(prompt_input) == x:
-                    found_field, found_field_list = get_field(df, field)
+                    found_field, found_field_list, found_field_str = get_field(df, field)
                     while True:
-                        op = input("\n[P] PRINT [G] GRAPH : ")
+                        op = input("\n" + found_field_str.strip() + " - [P] PRINT [G] GRAPH : ")
                         if op:
                             if op == 'p' or op == 'P':
                                 print_field(found_field, field)
@@ -177,20 +177,98 @@ def search_fields(df):
                             if op == 'g' or op == 'G':
                                 graph_field(df, field, found_field_list)
                                 break
+                        print("\nCommand not found....\n")
             except Exception:
                 prompt_input = prompt_input
         if len(found_field_list) == 0:
-            print("Logging field not found...")
+            print("\nInvalid index...")
+
+def print_fields(df):
+    print("\n --------------")
+    print("| PRINT FIELDS |")
+    print(" --------------\n")
+    print_all_fields(df)
+    found_field = ""
+    found_field_list = ""
+    while True:
+        prompt_input = input("\nENTER FIELD INDICES (COMMA SEPARATED) : ")
+        prompt_input = prompt_input.replace(" ", "")
+        prompt_input = [int(x) for x in prompt_input.split(',') if x.strip().isdigit()]
+        print()
+        if len(prompt_input) == 0:
+            print("Invalid indices...")
+            continue
+        if int(max(prompt_input, key = int)) > 48 or int(max(prompt_input, key = int)) < 0:
+            print("Invalid indices...")
+            continue
+        field_names = []
+        print_fields = []
+        for prompt_field in prompt_input:
+            for x, field in enumerate(all_logging_fields):
+                try:
+                    if int(prompt_field) == x:
+                        found_field, found_field_list, found_field_str = get_field(df, field)
+                        field_names.append(field)
+                        print_fields.append(found_field_list)
+                except Exception:
+                    prompt_input = prompt_input
+        header = ""
+        for name in field_names:
+            header += "{:<15} ".format(name)
+        print(header)
+        for i in range(len(print_fields[0])):
+            printout = ""
+            for j in range(len(print_fields)):
+                printout += "{:<15} ".format(np.array(print_fields[j])[i])
+            print(printout)
+
+def graph_fields(df):
+    print("\n --------------")
+    print("| GRAPH FIELDS |")
+    print(" --------------\n")
+    print_all_fields(df)
+    found_field = ""
+    found_field_list = ""
+    while True:
+        prompt_input = input("\nENTER FIELD INDICES (COMMA SEPARATED) : ")
+        prompt_input = prompt_input.replace(" ", "")
+        prompt_input = [int(x) for x in prompt_input.split(',') if x.strip().isdigit()]
+        print()
+        if len(prompt_input) == 0:
+            print("\nInvalid indices...")
+            continue
+        if int(max(prompt_input, key = int)) > 48 or int(max(prompt_input, key = int)) < 0:
+            print("\nInvalid indices...")
+            continue
+        field_names = []
+        print_fields = []
+        for prompt_field in prompt_input:
+            for x, field in enumerate(all_logging_fields):
+                try:
+                    if int(prompt_field) == x:
+                        found_field, found_field_list, found_field_str = get_field(df, field)
+                        field_names.append(field)
+                        print_fields.append(found_field_list)
+                except Exception:
+                    prompt_input = prompt_input
+        header = ""
+        for name in field_names:
+            header += "{:<15} ".format(name)
+        print(header)
+        for i in range(len(print_fields[0])):
+            printout = ""
+            for j in range(len(print_fields)):
+                printout += "{:<15} ".format(np.array(print_fields[j])[i])
+            print(printout)
 
 def graph_field(df, field_name, field_list):
     fig, ax = plt.subplots(figsize=(10, 8))
-    time_field, time_field_list = get_field(df, "Time")
+    time_field, time_field_list, time_field_str = get_field(df, "Time")
     x = [datetime.strptime(i, "%S.%f").second for i in time_field_list[1:]]
     y = field_list[1:].astype(float)
-    d = {'Time': x, field_name: y}
+    d = {'\nTime': x, field_name + '\n': y}
     temp_df = pd.DataFrame(d)
-    sns.set_style("darkgrid")
-    sns.lineplot(x='Time', y=field_name, data=temp_df)
+    sns.lineplot(x='\nTime', y=field_name + '\n', data=temp_df)
     ax.set_xlim(0, int(float(max(time_field_list, key = float))) - 1)
     ax.set_xticks(range(0, int(float(max(time_field_list, key = float)))))
     plt.show()
@@ -200,28 +278,28 @@ def get_field(df, field):
         columnName = str(columnName).replace(" ", "")
         columnName = columnName.replace('"', "")
         if str(columnName) == str(field):
-            print("\n* Found field: [" + str(i) + "] " + str(columnName))
+            found_field_str = "\n[" + str(i) + "] " + str(columnName).upper() + "\n"
             field = columnData.values[2:]
             field_list = str(field).replace(" ", ", ")
             field_list = str(field_list).replace("'", "")
             field_list = str(field_list).replace("\n", "")
             field_list = field_list.strip('][').split(', ')
             field_list = np.array(field_list)
-            return field, field_list
+            return field, field_list, found_field_str
     return "ERROR"
 
 def print_field(column, field):
-    print("\n* Printing field: " + str(field))
+    print()
     col_length = len(column)
     for val in range(0, col_length):
-        print("  {:<7} {:<25} {}".format("", column[val], field))
+        print(column[val])
 
 def print_all_fields(df):
     columns = collections.defaultdict(list)
     for i, (columnName, columnData) in enumerate(df.iteritems()):
         columnName = str(columnName).replace(" ", "")
         columnName = columnName.replace('"', "")
-        print("{:<10} {}".format("[" + str(i) + "]", columnName))
+        print("{:<15} {}".format("[" + str(i) + "]", columnName))
         columns[columnName].append(columnData.values)
 
 def plot(df):
