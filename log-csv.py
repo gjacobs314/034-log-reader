b02da58e9b078b88d98d3ec16e406da16b1d6cc5
Gordon Jacobs <34612258+gjacobs314@users.noreply.github.com>
2022-03-04 11:06:22 -0500

    Progress

--- log-csv.py
+++ log-csv.py
@@ -1,3 +1,4 @@
+import enum
 import sys
 import time
 from datetime import datetime
@@ -7,17 +8,7 @@ import collections
 import matplotlib.pyplot as plt
 import numpy as np
 
-def main():
-    if len(sys.argv) < 3 and len(sys.argv) >= 2:
-        df = pd.read_csv(sys.argv[1])
-        setup(df)
-    else:
-        print("No logfile specified, exiting...")
-        time.sleep(2)
-        exit(0)
-
-def setup(df):
-    all_logging_fields = [
+all_logging_fields = [
        "Time",
        "Milliseconds",
        "tia",
@@ -69,56 +60,56 @@ def setup(df):
        "CommandedThrottleActuatorControl"
     ]
 
-    fuel_fields = [
-        "Time",
-        "Milliseconds",
-        "fup",
-        "fup_sp",
-        "pump_vol_vcv",
-        "efppwm",
-        "fup_efp",
-        "ShortTermFuelTrim-Bank1",
-        "LongTermFuelTrim-Bank1",
-        "ShortTermFuelTrim-Bank2",
-        "LongTermFuelTrim-Bank2",
-        "EngineRPM",
-        "CommandedThrottleActuatorControl"
-    ]
+fuel_fields = [
+    "Time",
+    "Milliseconds",
+    "fup",
+    "fup_sp",
+    "pump_vol_vcv",
+    "efppwm",
+    "fup_efp",
+    "ShortTermFuelTrim-Bank1",
+    "LongTermFuelTrim-Bank1",
+    "ShortTermFuelTrim-Bank2",
+    "LongTermFuelTrim-Bank2",
+    "EngineRPM",
+    "CommandedThrottleActuatorControl"
+]
 
-    air_fields = [
-        "Time",
-        "Milliseconds",
-        "tia",
-        "TIA_AM_SCHA",
-        "tqi_gs_fast_dec",
-        "tqi_gs_fast_inc",
-        "pv_av",
-        "LAMB_LS_UP[1]",
-        "LAMB_LS_UP[2]",
-        "state_eng",
-        "teg_dyn_up_cat[1]",
-        "TEG_DYN_UP_CAT[2]",
-        "amp_mes",
-        "map",
-        "map_mes",
-        "map_1_mes",
-        "map_2_mes",
-        "pdt_mes",
-        "maf",
-        "lamb_sp[1]",
-        "LAMB_SP[2]",
-        "tqi_av",
-        "gear",
-        "vs",
-        "CAM_SP_IVVT_IN",
-        "map_sp",
-        "rfp_sp",
-        "EngineRPM",
-        "Ambientairtemperature",
-        "CommandedThrottleActuatorControl"
-    ]
+air_fields = [
+    "Time",
+    "Milliseconds",
+    "tia",
+    "TIA_AM_SCHA",
+    "tqi_gs_fast_dec",
+    "tqi_gs_fast_inc",
+    "pv_av",
+    "LAMB_LS_UP[1]",
+    "LAMB_LS_UP[2]",
+    "state_eng",
+    "teg_dyn_up_cat[1]",
+    "TEG_DYN_UP_CAT[2]",
+    "amp_mes",
+    "map",
+    "map_mes",
+    "map_1_mes",
+    "map_2_mes",
+    "pdt_mes",
+    "maf",
+    "lamb_sp[1]",
+    "LAMB_SP[2]",
+    "tqi_av",
+    "gear",
+    "vs",
+    "CAM_SP_IVVT_IN",
+    "map_sp",
+    "rfp_sp",
+    "EngineRPM",
+    "Ambientairtemperature",
+    "CommandedThrottleActuatorControl"
+]
 
-    ignition_fields = [
+ignition_fields = [
         "Time",
         "Milliseconds",
         "tia",
@@ -134,8 +125,107 @@ def setup(df):
         "IgnitionTimingAdvancefor#1Cylinder",
         "CommandedThrottleActuatorControl"
     ]
+
+def main():
+    if len(sys.argv) < 3 and len(sys.argv) >= 2:
+        df = pd.read_csv(sys.argv[1])
+        loop(df)
+    else:
+        print("No logfile specified, exiting...")
+        time.sleep(2)
+        exit(0)
+
+def loop(df):
+    print(" ________________________")
+    print("|                        |")
+    print("| 034 Log Analyzer Shell |")
+    print("|________________________|")
+    while True:
+        found_field = ""
+        found_field_list = ""
+        prompt_input = input("\n[S] SEARCH [P] PRINT [G] GRAPH : ")
+        lower = prompt_input.lower()
+        if lower == "all":
+            print_all_fields(df)
+            continue
+        if prompt_input.lower() == "s":
+            search_fields(df)
+            continue
+        print("\nCommand not found...")
     
-    fig, ax = plt.subplots(figsize=(9, 7))
+def search_fields(df):
+    print("\n ---------------")
+    print("| SEARCH FIELDS |")
+    print(" ---------------\n")
+    print_all_fields(df)
+    found_field = ""
+    found_field_list = ""
+    while True:
+        prompt_input = input("\n[X] ENTER FIELD INDEX: ")
+        for x, field in enumerate(all_logging_fields):
+            if prompt_input == field:
+                found_field, found_field_list = get_field(df, field)
+            try:
+                if int(prompt_input) == x:
+                    found_field, found_field_list = get_field(df, field)
+                    while True:
+                        op = input("\n[P] PRINT [G] GRAPH : ")
+                        if op:
+                            if op == 'p' or op == 'P':
+                                print_field(found_field, field)
+                                break
+                            if op == 'g' or op == 'G':
+                                graph_field(df, field, found_field_list)
+                                break
+            except Exception:
+                prompt_input = prompt_input
+        if len(found_field_list) == 0:
+            print("Logging field not found...")
+
+def graph_field(df, field_name, field_list):
+    fig, ax = plt.subplots(figsize=(10, 8))
+    time_field, time_field_list = get_field(df, "Time")
+    x = [datetime.strptime(i, "%S.%f").second for i in time_field_list[1:]]
+    y = field_list[1:].astype(float)
+    d = {'Time': x, field_name: y}
+    temp_df = pd.DataFrame(d)
+    sns.set_style("darkgrid")
+    sns.lineplot(x='Time', y=field_name, data=temp_df)
+    ax.set_xlim(0, int(float(max(time_field_list, key = float))) - 1)
+    ax.set_xticks(range(0, int(float(max(time_field_list, key = float)))))
+    plt.show()
+
+def get_field(df, field):
+    for i, (columnName, columnData) in enumerate(df.iteritems()):
+        columnName = str(columnName).replace(" ", "")
+        columnName = columnName.replace('"', "")
+        if str(columnName) == str(field):
+            print("\n* Found field: [" + str(i) + "] " + str(columnName))
+            field = columnData.values[2:]
+            field_list = str(field).replace(" ", ", ")
+            field_list = str(field_list).replace("'", "")
+            field_list = str(field_list).replace("\n", "")
+            field_list = field_list.strip('][').split(', ')
+            field_list = np.array(field_list)
+            return field, field_list
+    return "ERROR"
+
+def print_field(column, field):
+    print("\n* Printing field: " + str(field))
+    col_length = len(column)
+    for val in range(0, col_length):
+        print("  {:<7} {:<25} {}".format("", column[val], field))
+
+def print_all_fields(df):
+    columns = collections.defaultdict(list)
+    for i, (columnName, columnData) in enumerate(df.iteritems()):
+        columnName = str(columnName).replace(" ", "")
+        columnName = columnName.replace('"', "")
+        print("{:<10} {}".format("[" + str(i) + "]", columnName))
+        columns[columnName].append(columnData.values)
+
+def plot(df):
+    fig, ax = plt.subplots(figsize=(10, 8))
     time_field, time_field_list = get_field(df, "Time")
     x = [datetime.strptime(i, "%S.%f").second for i in time_field_list[1:]]
     engine_field, engine_field_list = get_field(df, "EngineRPM")
@@ -148,7 +238,7 @@ def setup(df):
     ax.set_xticks(range(0, int(float(max(time_field_list, key = float)))))
     plt.show()
 
-    fig, axs = plt.subplots(2, 2, figsize=(9, 7))
+    fig, axs = plt.subplots(2, 2, figsize=(12, 8))
     time_field, time_field_list = get_field(df, "Time")
     x = [datetime.strptime(i, "%S.%f") for i in time_field_list[1:]]
     engine_field, engine_field_list = get_field(df, "EngineRPM")
@@ -161,10 +251,10 @@ def setup(df):
     axs[0, 1].plot(x, y2, 'tab:orange')
     axs[0, 1].set(xlabel='', ylabel='Intake Air Temp')
     axs[0, 1].set_xticklabels([])
-    map_field, map_field_list = get_field(df, "map_mes")
+    map_field, map_field_list = get_field(df, "CommandedThrottleActuatorControl")
     y3 = map_field_list[1:].astype(float)
     axs[1, 0].plot(x, y3, 'tab:green')
-    axs[1, 0].set(xlabel='', ylabel='Boost\n')
+    axs[1, 0].set(xlabel='', ylabel='Throttle\n')
     axs[1, 0].set_xticklabels([])
     timing_advance_field, timing_advance_field_list = get_field(df, "IgnitionTimingAdvancefor#1Cylinder")
     y4 = timing_advance_field_list[1:].astype(float)
@@ -173,35 +263,7 @@ def setup(df):
     axs[1, 1].set_xticklabels([])
     plt.show()
 
-    #for field in air_fields:
-    #    found_field, found_field_list = get_field(df, field)
-    #    print_field(found_field, field)
-
-def get_field(df, field):
-    for i, (columnName, columnData) in enumerate(df.iteritems()):
-        columnName = str(columnName).replace(" ", "")
-        columnName = columnName.replace('"', "")
-        if str(columnName) == str(field):
-            print("* Found field: [" + str(i) + "] " + str(columnName))
-            field = columnData.values[2:]
-            field_list = str(field).replace(" ", ", ")
-            field_list = str(field_list).replace("'", "")
-            field_list = str(field_list).replace("\n", "")
-            field_list = field_list.strip('][').split(', ')
-            field_list = np.array(field_list)
-            return field, field_list
-    return "ERROR"
-
-def print_field(column, field):
-    print("* Printing field: " + str(field))
-    col_length = len(column)
-    for val in range(0, col_length):
-        print("  {:<7} {:<25} {}".format("", column[val], field))
-        #time.sleep(0.0025)
-
-def print_all_fields(df):
-    print()
-    print("{:<5}\n".format("[ALL LOGGING FIELDS]"))
+def print_all(df):
     columns = collections.defaultdict(list)
     for i, (columnName, columnData) in enumerate(df.iteritems()):
         columnName = str(columnName).replace(" ", "")
